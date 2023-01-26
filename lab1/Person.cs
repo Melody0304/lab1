a48c67017601f2f6413f8fa20d7b6e8b0ff2e357
马语晴 <@DESKTOP-FH7TEEO>
2023-01-26 09:52:04 +0800

    添加项目文件。

---
+++ lab1/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Person
+    {
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColor;
+        private int age;
+        private bool isWorking;
+
+        public Person(int personId, string firstName, string lastName, string favoriteColor, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColor = favoriteColor;
+            this.age = age;
+            this.isWorking = isWorking;
+        } 
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine(personId +":"+ firstName + " " + lastName + "’s favorite colour is " + favoriteColor);
+        }
+
+
+        public override string ToString()
+        {
+            return ($"personId:{personId}\n firstName: {firstName}\n lastName:{lastName} \nfavoriteColo:{favoriteColor}\nage: {age}\nIsWorking:{isWorking}");
+        }
+
+        public void ChangeFavoriteColour()
+        {
+            this.favoriteColor = "white";
+        }
+
+        public void GetAgeInTenYears()
+        {
+            this.age = age + 10;
+            Console.WriteLine(firstName + " " + lastName + "’s Age in 10 years is:" +  38);
+        }
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person peron1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person peron2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person peron3 = new Person(3, "Mike", " Briscoe", " Blue", 45, true);
+            Person peron4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+
+            peron1.DisplayPersonInfo();
+            Console.WriteLine(peron1.ToString());
+            peron1.ChangeFavoriteColour(); ;
+            Console.WriteLine(peron2.ToString());
+            peron2.GetAgeInTenYears();
+            Console.WriteLine(peron3.ToString());
+            Console.WriteLine(peron4.ToString());
+            
+            peron1.DisplayPersonInfo();
+            
+
+
+        }
+    }
+}
